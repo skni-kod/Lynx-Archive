72e112b3da598ef7695586b7adc437c1bc8a229f
Jakub Przystasz <jakub.przystasz@gmail.com>
2021-09-06 11:05:01 +0200

    Make HelloWorld! window

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -1,128 +1,83 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
-
+ 
 #include <xcb/xcb.h>
-
-#include <X11/Xlib.h>
-
-
-/*
-  To use it on windows 10 wsl you need to install:
-  X's server for windows eg: https://sourceforge.net/projects/xming/
-  in wsl shell where you run program, execute that command:
-  export DISPLAY=:0
-  and before start you need to open X's server session on your computer!
-*/
-
-double
-get_time(void)
+ 
+int main ()
 {
-  struct timeval timev;
-
-  gettimeofday(&timev, NULL);
-
-  return (double)timev.tv_sec + (((double)timev.tv_usec) / 1000000);
-}
-
-int main()
-{
-  xcb_connection_t *c;
-  xcb_atom_t *atoms;
-  xcb_intern_atom_cookie_t *cs;
-  char **names;
-  int count;
-  int i;
-  double start;
-  double end;
-  double diff;
-
-  /* Xlib */
-  Display *disp;
-  Atom *atoms_x;
-  double diff_x;
-
-  c = xcb_connect(NULL, NULL);
-
-  count = 500;
-  atoms = (xcb_atom_t *)malloc(count * sizeof(atoms));
-  names = (char **)malloc(count * sizeof(char *));
-
-  /* init names */
-  for (i = 0; i < count; ++i)
-  {
-    char buf[100];
-
-    sprintf(buf, "NAME%d", i);
-    names[i] = strdup(buf);
+  xcb_connection_t    *c;
+  xcb_screen_t        *screen;
+  xcb_drawable_t       win;
+  xcb_gcontext_t       foreground;
+  xcb_gcontext_t       background;
+  xcb_generic_event_t *e;
+  uint32_t             mask = 0;
+  uint32_t             values[2];
+ 
+  char string[] = "Hello, XCB!";
+  uint8_t string_len = strlen(string);
+ 
+  xcb_rectangle_t rectangles[] = {
+    {40, 40, 20, 20},
+  };
+ 
+  c = xcb_connect (NULL, NULL);
+ 
+  /* get the first screen */
+  screen = xcb_setup_roots_iterator (xcb_get_setup (c)).data;
+ 
+  /* root window */
+  win = screen->root;
+ 
+  /* create black (foreground) graphic context */
+  foreground = xcb_generate_id (c);
+  mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
+  values[0] = screen->black_pixel;
+  values[1] = 0;
+  xcb_create_gc (c, foreground, win, mask, values);
+ 
+  /* create white (background) graphic context */
+  background = xcb_generate_id (c);
+  mask = XCB_GC_BACKGROUND | XCB_GC_GRAPHICS_EXPOSURES;
+  values[0] = screen->white_pixel;
+  values[1] = 0;
+  xcb_create_gc (c, background, win, mask, values);
+ 
+  /* create the window */
+  win = xcb_generate_id(c);
+  mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+  values[0] = screen->white_pixel;
+  values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
+  xcb_create_window (c,                             /* connection    */
+                     XCB_COPY_FROM_PARENT,          /* depth         */
+                     win,                           /* window Id     */
+                     screen->root,                  /* parent window */
+                     0, 0,                          /* x, y          */
+                     150, 150,                      /* width, height */
+                     10,                            /* border_width  */
+                     XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class         */
+                     screen->root_visual,           /* visual        */
+                     mask, values);                 /* masks         */
+ 
+  /* map the window on the screen */
+  xcb_map_window (c, win);
+ 
+  xcb_flush (c);
+ 
+  while ((e = xcb_wait_for_event (c))) {
+    switch (e->response_type & ~0x80) {
+    case XCB_EXPOSE:
+      xcb_poly_rectangle (c, win, foreground, 1, rectangles);
+      xcb_image_text_8 (c, string_len, win, background, 20, 20, string);
+      xcb_flush (c);
+      break;
+    case XCB_KEY_PRESS:
+      goto endloop;
+    }
+    free (e);
   }
-
-  /* bad use */
-  start = get_time();
-
-  for (i = 0; i < count; ++i)
-    atoms[i] = xcb_intern_atom_reply(c,
-                                     xcb_intern_atom(c,
-                                                     0,
-                                                     strlen(names[i]),
-                                                     names[i]),
-                                     NULL)
-                   ->atom;
-
-  end = get_time();
-  diff = end - start;
-  printf("bad use time  : %f\n", diff);
-
-  /* good use */
-  start = get_time();
-
-  cs = (xcb_intern_atom_cookie_t *)malloc(count * sizeof(xcb_intern_atom_cookie_t));
-  for (i = 0; i < count; ++i)
-    cs[i] = xcb_intern_atom(c, 0, strlen(names[i]), names[i]);
-
-  for (i = 0; i < count; ++i)
-  {
-    xcb_intern_atom_reply_t *r;
-
-    r = xcb_intern_atom_reply(c, cs[i], 0);
-    if (r)
-      atoms[i] = r->atom;
-    free(r);
-  }
-
-  end = get_time();
-  printf("good use time : %f\n", end - start);
-  printf("ratio         : %f\n", diff / (end - start));
-  diff = end - start;
-
-  /* free var */
-  free(atoms);
-  free(cs);
-
-  xcb_disconnect(c);
-
-  /* Xlib */
-  disp = XOpenDisplay(getenv("DISPLAY"));
-
-  atoms_x = (Atom *)malloc(count * sizeof(atoms_x));
-
-  start = get_time();
-
-  for (i = 0; i < count; ++i)
-    atoms_x[i] = XInternAtom(disp, names[i], 0);
-
-  end = get_time();
-  diff_x = end - start;
-  printf("Xlib use time : %f\n", diff_x);
-  printf("ratio         : %f\n", diff_x / diff);
-
-  free(atoms_x);
-  for (i = 0; i < count; ++i)
-    free(names[i]);
-  free(names);
-
-  XCloseDisplay(disp);
-
+  endloop:
+ 
   return 0;
 }
